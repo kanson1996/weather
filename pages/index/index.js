bb8f862fc37e4d2232b0a02bf889d2698201d25a
kanson1996 <kanson1996@gmail.com>
2018-05-03 14:38:41 +0800

    update 7 days work for cn-weather learning from course 4 to course 6

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,177 @@
 //index.js
-//获取应用实例
-const app = getApp()
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+
+const weatherColorMap = {
+ 'sunny': '#cbeefd',
+ 'cloudy': '#deeef6',
+ 'overcast': '#c6ced2',
+ 'lightrain': '#bdd5e1',
+ 'heavyrain': '#c5ccd0',
+ 'snow': '#aae1fc'
+}
+
+const QQMapWX = require("../../libs/qqmap-wx-jssdk.js")
+
+const UNPROMPTED = 0
+const UNAUTHORIZED = 1
+const AUTHORIZED = 2
+
+const UNPROMPTED_TIPS = "点击获取当前位置"
+const UNAUTHORIZED_TIPS = "点击开启位置权限"
+const AUTHORIZED_TIPS = ""
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    nowTemp: '',
+    nowWeather: '',
+    nowWeatherBackground: '',
+    hourlyWeather: [],
+    todayTemp: "",
+    todayDate: "",
+    city: "杭州市",
+    locationAuthType: UNPROMPTED,
+    locationTipsText: UNPROMPTED_TIPS
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
+  onPullDownRefresh(){
+    console.log('onPullDownRefresh')
+    this.getNow(()=>{
+      wx.stopPullDownRefresh()
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
+  onLoad(){
+    console.log('onLoad')
+    this.qqmapsdk = new QQMapWX({
+      key: "ML2BZ-VJLWG-OTZQR-IEGHP-S2P2E-J5BDV"
+    })
+    this.getNow()
+  },
+
+  onShow(){
+    console.log('onShow')
+    wx.getSetting({
+      success: res=>{
+        let auth = res.authSetting["scope.userLocation"]
+        if(auth && this.data.locationAuthType !== AUTHORIZED) {
+          //权限从无到有
           this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
+            locationAuthType: AUTHORIZED,
+            locationTipsText: AUTHORIZED_TIPS
+          })
+          this.getLocation()
+        }
+        //权限从有到无处理
+
+      }
+    })
+  },
+  getNow(callback){
+      wx.request({
+      url:
+      'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: this.data.city
+      },
+      success: res => {
+        console.log(res)
+        let result = res.data.result
+        this.setNow(result)
+        this.setHourlyWeather(result)
+        this.setToday(result)
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
+  },
+  setNow(result){
+    let temp = result.now.temp
+    let weather = result.now.weather
+    console.log(temp, weather)
+    this.setData({
+      nowTemp: temp + '°',
+      nowWeather: weatherMap[weather],
+      nowWeatherBackground: '/images/' + weather + '-bg.png'
+    })
+    wx.setNavigationBarColor({
+      frontColor: '#000000',
+      backgroundColor: weatherColorMap[weather],
+    })
+  },
+  setHourlyWeather(result){
+    //set forecast
+    let forecast = result.forecast
+    let nowHour = new Date().getHours()
+    let hourlyWeather = []
+    for (let i = 0; i < 8; i++) {
+      hourlyWeather.push({
+        time: (i * 3 + nowHour) % 24 + "时",
+        iconPath: "/images/" + forecast[i].weather + "-icon.png",
+        temp: forecast[i].temp + "°"
+      })
+    }
+    hourlyWeather[0].time = "现在"
+    this.setData({
+      hourlyWeather: hourlyWeather
+    })
+  },
+  setToday(result){
+    let date = new Date()
+    this.setData({
+      todayTemp: `${result.today.minTemp}° - ${result.today.maxTemp}°`,
+      todayDate: `${date.getFullYear()}-${date.getMonth()}-${date.getDate()} 今天`
+    })
+  },
+  onTapDayWeather(){
+    console.log('onTapDayWeather')
+    wx.showToast()
+    wx.navigateTo({
+      url: "/pages/list/list?city=" + this.data.city
+    })
+  },
+  onTapLocation(){
+    console.log('onTapLocation')
+    if(this.data.locationAuthType === UNAUTHORIZED)
+      wx.openSetting()
+    else
+      this.getLocation()
+  },
+  getLocation(){
+      wx.getLocation({
+        type: 'wgs84',
+        success: res => {
+          this.setData({
+            locationAuthType: AUTHORIZED,
+            locationTipsText: AUTHORIZED_TIPS
+          })
+          this.qqmapsdk.reverseGeocoder({
+            location: {
+              latitude: res.latitude,
+              longitude: res.longitude
+            },
+            success: res => {
+              let city = res.result.address_component.city
+              console.log(city)
+              this.setData({
+                city: city,
+                locationTipsText: ""
+              })
+              this.getNow()
+            }
+          })
+        },
+        fail: () => {
+          this.setData({
+            locationAuthType: UNAUTHORIZED,
+            locationTipsText: UNAUTHORIZED_TIPS
           })
         }
       })
     }
-  },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
 })
---
+++ pages/list/list.js
@@ -0,0 +1,60 @@
+// pages/list/list.js
+const dayMap = ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六"]
+Page({
+  date: {
+    weekWeather: [],
+    city: "杭州市"
+  },
+  onLoad(options){
+    console.log("onLoad")
+    this.setData({
+      city: options.city
+    })
+    this.getWeekWeather()
+
+  },
+  onPullDownRefresh(){
+    console.log("onPullDownRefresh")
+    this.getWeekWeather(()=> {
+      wx.stopPullDownRefresh()
+    })
+  },
+  getWeekWeather(callback){
+    wx.request({
+      url:
+      'https://test-miniprogram.com/api/weather/future',
+      data: {
+        time: new Date().getTime(),
+        city: this.data.city
+      },
+      success: res=> {
+        console.log(res)
+        let result = res.data.result
+        this.setWeekWeather(result)
+        //this.setWeekWeather(result)
+      },
+      complete: ()=> {
+        callback && callback()
+      }
+    })
+  },
+  setWeekWeather(result){
+    let weekWeather = []
+    for(let i = 0; i < 7; i++){
+      let date = new Date()
+      date.setDate(date.getDate() + i)
+      weekWeather.push({
+        day: dayMap[date.getDay()],
+        date: `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`,
+        temp: `${result[i].minTemp}° - ${result[i].maxTemp}°`,
+        iconPath: '/images/' + result[i].weather + '-icon.png'
+      })
+    }
+    weekWeather[0].day = "今天"
+    weekWeather[1].day = "明天"
+    this.setData({
+      weekWeather
+    })
+  }
+
+})
